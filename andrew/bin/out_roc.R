9e2f82cedd8bdaa036036981e5fc480472fc0561
aph2126 <aph2126@columbia.edu>
2014-05-02 22:03:44 -0400

    Updated ROC curves

--- andrew/bin/out_roc.R
+++ andrew/bin/out_roc.R
@@ -14,48 +14,71 @@ y.train = ad.train$TotalConversions
 library(ROCR)
 
 
-# Loading predicted values
-filenames = list.files(path.output, "yhat", full.names = TRUE)
-predictions = list()
-for (i in 1:length(filenames)) {
-    n1 = gsub(".*\\yhat_(.*)\\.Rda", "\\1", filenames[1])
-    n2 = paste("pred.", n1, sep = "")
-    assign(n2, predi
-}
-
-filenames = dir(dirname,full.names=TRUE)
-
-
-# Loading SVM results
+# Loading predicted values, appending
 setwd(path.output)
 load("yhat_sSTEP.Rda")
-pred.svm.train = prediction(yhat.svm.train, y.train)
-pred.svm.test = prediction(yhat.svm.test, y.test)
-auc.svm.train = performance(pred.svm.train, 'auc')
-auc.svm.test = performance(pred.svm.test, 'auc')
-roc.svm.train = performance(pred.svm.train, "tpr", "fpr" )
-roc.svm.test = performance(pred.svm.test, "tpr", "fpr" )
-
-# Loading Random Forest results
-setwd(path.output)
+load("yhat_logLASSO.Rda")
 load("yhat_rForest.Rda")
-pred.rForest.train = prediction(yhat.rForest.train, y.train)
-pred.rForest.test = prediction(yhat.rForest.test, y.test)
-auc.rForest.train = performance(pred.rForest.train, 'auc')
-auc.rForest.test = performance(pred.rForest.test, 'auc')
-roc.rForest.train = performance(pred.rForest.train, "tpr", "fpr" )
-roc.rForest.test = performance(pred.rForest.test, "tpr", "fpr" )
+load("yhat_svm.Rda")
+yhat.svm.test = 1 - yhat.svm.test
+yhat.svm.train = 1 - yhat.svm.train
+yhat.train = cbind(yhat.sSTEP.train, yhat.logLASSO.train, yhat.rForest.train, yhat.svm.train)
+yhat.test = cbind(yhat.sSTEP.test, yhat.logLASSO.test, yhat.rForest.test, yhat.svm.test)
+colnames(yhat.train) = colnames(yhat.test) = c("Step", "LASSO", "Random Forest", "SVM")
 
 
-# ROC Curve
-y = unlist(roc.rForest.test@y.values)
-x = unlist(roc.rForest.test@x.values)
-par(mar = c(3,3,0,0) + .1)
-plot(x, y, type = "l", axes = F, xlab = "", ylab = "", lwd = .5)
-lines(x1, y1, type = "l", col = 2, lwd = .5)
-axis(side = 1, tck = -.015, line = -.4, cex.axis = .8)
-axis(side = 2, tck = -.015, line = -.4, cex.axis = .8, las = 2)
-mtext(side = 1, roc.lasso@x.name, line = 2, cex = .8)
-mtext(side = 2, roc.lasso@y.name, line = 2, cex = .8)
+# Performance statistics
+fp.tp.train = fp.tp.test = list()
+for (i in 1:ncol(yhat.train)) {
+    pred.train.i = prediction(yhat.train[,i], y.train)
+    pred.test.i = prediction(yhat.test[,i], y.test)
+    perf.train.i = performance(pred.train.i,"tpr","fpr")
+    perf.test.i = performance(pred.test.i,"tpr","fpr")
+    fp.tp.train[[i]] = cbind(unlist(perf.train.i@x.values), unlist(perf.train.i@y.values))
+    fp.tp.test[[i]] = cbind(unlist(perf.test.i@x.values), unlist(perf.test.i@y.values))
+}
 
+
+# ROC
+n.cex = .7
+setwd(path.output)
+setwd("../paper/")
+setEPS()
+postscript("fig_roc.eps", height = 3, width = 6)
+mat = rbind(c(1,2,3,4),c(5,6,7,8), c(9,9,9,9))
+layout(mat, heights = c(.45,.55,.1), widths = c(1.1,.8,.8,.8))
+
+par(mar = c(.5,4,0,0)+.1)
+plot(x = fp.tp.train[[1]][,1], y = fp.tp.train[[1]][,2], type = "l", axes = F, ylab = "", xlab = "", col = 1)
+axis(side = 1, tck = -.015, labels = NA)
+axis(side = 2, tck = -.015, labels = seq(0,1,by=.25), at = seq(0,1,by=.25), las = 2, cex = n.cex)
+mtext(side = 2, "TP (Train)", line = 3, cex = n.cex)
+
+par(mar = c(.5,0,0,0)+.1)
+for (i in 2:ncol(yhat.train)) {
+    plot(x = fp.tp.train[[i]][,1], y = fp.tp.train[[i]][,2], type = "l", axes = F, ylab = "", xlab = "", col = i)
+    axis(side = 1, tck = -.015, labels = NA)
+    axis(side = 2, tck = -.015, labels = NA)
+}
+
+par(mar = c(3,4,0,0)+.1)
+plot(x = fp.tp.test[[1]][,1], y = fp.tp.test[[1]][,2], type = "l", axes = F, ylab = "", xlab = "", col = 1)
+axis(side = 1, tck = -.015, labels = seq(0,1,by=.25), at = seq(0,1,by=.25), las = 1, cex = n.cex)
+axis(side = 2, tck = -.015, labels = seq(0,1,by=.25), at = seq(0,1,by=.25), las = 2, cex = n.cex)
+mtext(side = 2, "TP (Test)", line = 3, cex = n.cex)
+mtext(side = 1, "FP", line = 2, cex = n.cex)
+
+par(mar = c(3,0,0,0)+.1)
+for (i in 2:ncol(yhat.train)) {
+    plot(x = fp.tp.test[[i]][,1], y = fp.tp.test[[i]][,2], type = "l", axes = F, ylab = "", xlab = "", col = i)
+    axis(side = 1, tck = -.015, labels = seq(0,1,by=.25), at = seq(0,1,by=.25), las = 1, cex = n.cex)
+    axis(side = 2, tck = -.015, labels = NA)
+    mtext(side = 1, "FP", line = 2, cex = n.cex)
+}
+
+par(mar = c(0,0,0,0))
+plot(1, type = "n", axes=FALSE, xlab="", ylab="")
+legend(x = "top",inset = 0,legend = c("Stepwise", "LASSO", "Random Forest", "RBF SVM"),
+       col = c(1:4), lwd = 1, cex = n.cex*1.2, horiz = TRUE, bty = "n")
+dev.off()
 
